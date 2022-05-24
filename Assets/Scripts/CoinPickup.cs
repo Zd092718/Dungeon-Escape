4fe44ac46f70f8306f9cb9851654b5f2c7b48b0a
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-24 14:23:41 -0600

    coin added and sfx applied

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickupSFX;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        }
+    }
+}
